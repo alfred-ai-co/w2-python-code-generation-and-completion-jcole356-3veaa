b5e119f850c26dce8e8b0656afa7e1f56424c622
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2024-10-15 19:49:21 -0700

    Add message class for 404 to include in Swagger docs

--- app/api/routes/projects.py
+++ app/api/routes/projects.py
@@ -1,36 +1,35 @@
 # Project Endpoints
 from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
+from app.api_models.message import Message
 from fastapi import Depends
 
 import app.db_models.crud as crud
 from app.api_models.projects import ProjectCreate, ProjectResponse
 from app.api.dependencies.sqldb import get_db
 
-
 router = APIRouter()
-
 
 @router.post('', response_model=ProjectResponse)
 async def create_project(project: ProjectCreate, db: Session = Depends(get_db)):
     db_project = crud.create_project(db, project.name, project.description)
     return db_project
 
-@router.get('/{project_id}', response_model=ProjectResponse)
+@router.get('/{project_id}', response_model=ProjectResponse, responses={404: {"model": Message}})
 async def read_project(project_id: int, db: Session = Depends(get_db)):
     db_project = crud.get_project(db, project_id)
     if not db_project:
         raise HTTPException(status_code=404, detail="Project not found")
     return db_project
 
-@router.put('/{project_id}', response_model=ProjectResponse)
+@router.put('/{project_id}', response_model=ProjectResponse, responses={404: {"model": Message}})
 async def update_project(project_id: int, project: ProjectCreate, db: Session = Depends(get_db)):
     db_project = crud.update_project(db, project_id, project.name, project.description)
     if not db_project:
         raise HTTPException(status_code=404, detail="Project not found")
     return db_project
 
-@router.delete('/{project_id}')
+@router.delete('/{project_id}', responses={404: {"model": Message}})
 async def delete_project(project_id: int, db: Session = Depends(get_db)):
     db_project = crud.delete_project(db, project_id)
     if not db_project:
