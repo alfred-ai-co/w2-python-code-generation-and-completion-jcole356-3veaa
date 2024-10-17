5f80360a1a5ab494a79d43527e1dc46c3a364915
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2024-10-16 20:04:45 -0700

    Add create_ticket operation

--- app/api_models/tickets.py
+++ app/api_models/tickets.py
@@ -3,12 +3,12 @@ from typing import Optional
 from datetime import datetime
 
 class TicketCreate(BaseModel):
-  project_id: int
-  title: str
-  description: Optional[str] = None
-  due_date: Optional[datetime] = Field(..., default=None, description="The date with which the ticket is due and can be flagged if current dates are past this")
-  status: Optional[str] = Field(..., default="open", description="The status of the ticket")
-  priority: Optional[str] = None
+    project_id: int
+    title: str
+    description: Optional[str] = None
+    due_date: Optional[datetime] = Field(..., default=None, description="The date with which the ticket is due and can be flagged if current dates are past this")
+    status: Optional[str] = Field(..., default="open", description="The status of the ticket")
+    priority: Optional[str] = None
 
 class TicketResponse(TicketCreate):
 	id: int
--- app/db_models/crud.py
+++ app/db_models/crud.py
@@ -1,5 +1,19 @@
 from sqlalchemy.orm import Session
 from app.db_models.base import Project, Ticket
+from typing import Optional
+from app.db_models.base import Ticket
 
 
 # CRUD operations for Project
+def create_ticket(db: Session, ticket_id: int, title: str, description: Optional[str] = None, status: Optional[str] = None, priority: Optional[str] = None) -> Ticket:
+    new_ticket = Ticket(
+        id=ticket_id,
+        title=title,
+        description=description,
+        status=status,
+        priority=priority
+    )
+    db.add(new_ticket)
+    db.commit()
+    db.refresh(new_ticket)
+    return new_ticket
