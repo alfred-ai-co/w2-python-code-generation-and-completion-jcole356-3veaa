1d02dda93698eb843b57c256a0bc6fdcbe1287a0
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2024-10-16 20:00:02 -0700

    Add Ticket models

--- app/api_models/tickets.py
+++ app/api_models/tickets.py
@@ -1,3 +1,15 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import datetime
+
+class TicketCreate(BaseModel):
+  project_id: int
+  title: str
+  description: Optional[str] = None
+  due_date: Optional[datetime] = Field(..., default=None, description="The date with which the ticket is due and can be flagged if current dates are past this")
+  status: Optional[str] = Field(..., default="open", description="The status of the ticket")
+  priority: Optional[str] = None
+
+class TicketResponse(TicketCreate):
+	id: int
+	created_at: datetime
